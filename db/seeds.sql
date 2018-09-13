2b1907d8e82768466b360819c5127a64af22f8d2
GHK15 <ginakim115@gmail.com>
2018-09-13 15:55:38 -0700

    functionality2

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom & Swiss Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut Butter Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ramen Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon Burger', true);
