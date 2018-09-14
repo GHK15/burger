74c50ca467a667ef992ed7f4666f8bdead772ace
GHK15 <ginakim115@gmail.com>
2018-09-14 16:04:34 -0700

    remove notes

--- db/schema.sql
+++ db/schema.sql
@@ -7,12 +7,6 @@ CREATE TABLE burgers
 	id INT AUTO_INCREMENT NOT NULL,
 	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-    -- date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
-
-
-ttjberwti2erppa1
-
-tsqade6awx7gktg5
